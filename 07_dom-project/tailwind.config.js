1c9afe58ab8a881ab16d73c7550e586ad4587105
Bishwajeet <115135717+Bishwajeet-07@users.noreply.github.com>
2023-11-02 13:20:38 +0530

    Add files via upload

---
+++ 07_dom-project/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html', "2-BMICalculator/*.html","3-DigitalClock/*.html","5-keyboard/*.html", "6-unlimitedColors/*.html",
+"4-GuessTheNumber/*.html", "to-do-app/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
